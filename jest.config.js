9371acf056e6d0118693725ba03d77d841350fb8
Aldehir Rojas <hello@alde.io>
2019-06-09 22:19:43 -0500

    Create a separate tsconfig for tsserver

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
+import { pathsToModuleNameMapper } from 'ts-jest/utils'
+import { compilerOptions } from './tsconfig'
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
